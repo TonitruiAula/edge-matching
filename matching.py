6cd288e89ca505fcbbe709fd40b164a31d3e19af
TonitruiAula <liu_boqian@sina.com>
2018-01-20 15:12:58 +0800

    sobel gradient direction and scale

--- matching.py
+++ matching.py
@@ -5,35 +5,45 @@ import time
 import sys
 import cv2
 import numpy as np
+import math
 
-def sob_bin(img, t_bin):
-    sobel_X = cv2.Sobel(img, cv2.CV_8UC1, 1, 0)
-    sobel_Y = cv2.Sobel(img, cv2.CV_8UC1, 0, 1)
-    sob = cv2.addWeighted(sobel_X, 0.5, sobel_Y, 0.5, 0)
-    dst = cv2.threshold(sob, t_bin, 255, cv2.THRESH_OTSU)
-    return dst
+def sob(img, t_bin):
+    sobelX = cv2.Sobel(img, cv2.CV_8UC1, 1, 0)
+    sobelY = cv2.Sobel(img, cv2.CV_8UC1, 0, 1)
+    sobAbs = cv2.addWeighted(sobelX, 0.5, sobelY, 0.5, 0)
+    sobDir = np.zeros(img.shape)
+    height = img.shape[0]
+    width = img.shape[1]
+    for i in xrange(height):
+        for j in xrange(width):
+            sobDir[i,j] = math.atan2(math.fabs(sobelY[i,j]), math.fabs(sobelX[i,j])) / (math.pi / 2)
+    # flag, dst = cv2.threshold(sob, t_bin, 255, cv2.THRESH_OTSU)
+    return [sobAbs, sobDir]
 
-def getEdge(imgpath_L, imgpath_R, t_bin):
-    img_L = cv2.imread(imgpath_L)
-    img_R = cv2.imread(imgpath_R)
-    gray_L = cv2.cvtColor(img_L, cv2.COLOR_BGR2GRAY)
-    gray_R = cv2.cvtColor(img_R, cv2.COLOR_BGR2GRAY)
-    edge_L = sob_bin(gray_L, t_bin)
-    edge_R = sob_bin(gray_R, t_bin)
-    return [edge_L, edge_R]
+def getEdge(imgpathL, imgpathR, t_bin):
+    imgL = cv2.imread(imgpathL)
+    imgR = cv2.imread(imgpathR)
+    grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+    grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+    edgeLa, edgeLd = sob(grayL, t_bin)
+    edgeRa, edgeRd = sob(grayR, t_bin)
+    return [edgeLa, edgeLd, edgeRa, edgeRd]
 
 
 
-def match1(imgpath_L, imgpath_R, t_bin, t_dir, t_abs, t_gray):
-    edge_L, edge_R = getEdge(imgpath_L, imgpath_R, t_bin)
-    cv2.imshow('left', edge_L)
-    cv2.imshow('right', edge_R)
+def match1(imgpathL, imgpathR, t_bin, t_dir, t_abs, t_gray):
+    edgeLa, edgeLd, edgeRa, edgeRd = getEdge(imgpathL, imgpathR, t_bin)
+    cv2.imshow('left', edgeLa)
+    cv2.imshow('right', edgeRa)
+    cv2.imshow('leftD', edgeLd)
+    np.savetxt('ld.txt',edgeLd,'%.3f')
+    np.savetxt('la.txt',edgeLa,'%d')    
+    cv2.imshow('rightD', edgeRd)
 
 
 if __name__ == '__main__':
-    imgpath_L = sys.argv[1]
-    imgpath_R = sys.argv[2]
+    imgpathL = sys.argv[1]
+    imgpathR = sys.argv[2]
     t_bin = float(sys.argv[3])
-    edge_L, edge_R = getEdge(imgpath_L, imgpath_R, t_bin)
-    cv2.imshow('left', edge_L)
-    cv2.imshow('right', edge_R)
+    match1(imgpathL,imgpathR,t_bin,0,0,0)
+    cv2.waitKey(0)
