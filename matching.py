8b5dea18a7ff782a01d0052f00293e94d1071b7a
TonitruiAula <liu_boqian@sina.com>
2018-01-23 13:57:10 +0800

    small range

--- matching.py
+++ matching.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 import math
 
+# 利用Sobel算子计算图片的梯度大小和方向
 def sob(img):
     sobelX = cv2.Sobel(img, cv2.CV_8UC1, 1, 0)
     sobelY = cv2.Sobel(img, cv2.CV_8UC1, 0, 1)
@@ -17,28 +18,36 @@ def sob(img):
     width = img.shape[1]
     for i in xrange(height):
         for j in xrange(width):
-            sobDir[i,j] = math.atan2(math.fabs(sobelY[i,j]), math.fabs(sobelX[i,j])) / (math.pi / 2) * 90
+            sobDir[i,j] = math.atan2(math.fabs(sobelY[i,j]), math.fabs(sobelX[i,j])) / (math.pi / 2.0) * 90.0
     # flag, dst = cv2.threshold(sob, t_bin, 255, cv2.THRESH_OTSU)
     return [sobAbs, sobDir]
 
+# 获取左右边缘
 def getEdge1(imgL, imgR):
-    assert(imgL.shape == imgR.shape)
+    # assert(imgL.shape == imgR.shape)
     grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
     grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
     edgeLa, edgeLd = sob(grayL)
     edgeRa, edgeRd = sob(grayR)
+    np.savetxt('la.txt',edgeLa,'%d')
+    np.savetxt('ld.txt',edgeLd,'%.1f')
+    np.savetxt('ra.txt',edgeRa,'%d')
+    np.savetxt('rd.txt',edgeRd,'%.1f')
     return [edgeLa, edgeLd, edgeRa, edgeRd]
 
+# 找到左视图对应的匹配点
 def findCores(grayL, grayR, edge, i, j, kL, seg_len, t_dir, t_abs, h_size=2):
     edgeLa, edgeLd, edgeRa, edgeRd = edge
+
     # 匹配梯度方向
     kRs = []
     for k in xrange(0,seg_len):
+        a = edgeLd[i,j+kL]
+        b = edgeRd[i,j+k]
         if math.fabs(edgeLd[i,j+kL] - edgeRd[i,j+k]) < t_dir:
             kRs.append(k)
 
     # 匹配梯度大小
-        # 窗口归一
     if len(kRs) == 0:
         return -1
     gvecL = []
@@ -47,7 +56,7 @@ def findCores(grayL, grayR, edge, i, j, kL, seg_len, t_dir, t_abs, h_size=2):
     for _i in xrange(i-h_size,i+h_size+1):
         for _j in xrange(j+kL-h_size,j+kL+h_size+1):
             gvecL.append(edgeLa[_i,_j])
-            ggrayL.append(grayL[i,j])
+            ggrayL.append(grayL[_i,_j])
 
     for k in kRs:
         gvecR = []
@@ -86,7 +95,7 @@ def match1(imgL, imgR, edge, seg_len, t_dir, t_abs, win_size=5):
     height = imgL.shape[0]
     width = imgL.shape[1]
     h_size = int(win_size)/2
-    disp = np.zeros(imgL.shape)
+    disp = np.zeros((height,width))
     for i in xrange(win_size,height-win_size):
         for j in xrange(win_size,width-win_size-seg_len,seg_len):
             maxGa = edgeLa[i,j]
@@ -100,18 +109,24 @@ def match1(imgL, imgR, edge, seg_len, t_dir, t_abs, win_size=5):
                     maxGa = edgeLa[i,j+k]
                     kL = k
             kR = findCores(grayL, grayR, edge, i, j, kL, seg_len, t_dir, t_abs, h_size)
+            if kR == -1:
+                continue
             ckL = findCores(grayR, grayL, edge, i, j, kR, seg_len, t_dir, t_abs, h_size)
             if kL == ckL:
                 disp[i,j+k] = math.fabs(kR-kL)
+                print '(',i,' , ',j+k,' , ',disp[i,j+k],')'
     
     maxDisp = disp.max()
-    disp = disp / float(maxDisp) * 255.0
-    disp.astype('uint8')
+    disp = disp / float(maxDisp)# * 255.0
+    # disp.astype('uint8')
     return maxDisp, disp
 
     
 
 if __name__ == '__main__':
+
+    print os.path.abspath(os.curdir)
+    print 'starting...'
 
     imgpathL = sys.argv[1]
     imgpathR = sys.argv[2]
@@ -119,22 +134,21 @@ if __name__ == '__main__':
     t_dir = float(sys.argv[4])
     t_abs = float(sys.argv[5])
 
-    # imgpathL = 'imL.png'
-    # imgpathR = 'imR.png'
-    # seg_len = 10
-    # t_dir = 0.5
-    # t_abs = 0.5
-
-
     imgL = cv2.imread(imgpathL)
     imgR = cv2.imread(imgpathR)
 
+    print imgL.shape
+
     edge = getEdge1(imgL,imgR)
 
+    ld = edge[1]/90.0
+    ld.astype('uint8')
+    rd = edge[3]/90.0
+    rd.astype('uint8')
     cv2.imshow('left grad abs',edge[0])
-    cv2.imshow('left grad dir',edge[1])
+    cv2.imshow('left grad dir',ld)
     cv2.imshow('right grad abs',edge[2])
-    cv2.imshow('right grad dir',edge[3])
+    cv2.imshow('right grad dir',rd)
 
     maxDisp, disp = match1(imgL, imgR, edge, seg_len, t_dir, t_abs)
     print 'max disparity: ' , maxDisp
